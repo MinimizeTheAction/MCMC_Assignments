45dfcb4ccbcda1c3e40c8cfb059e845ad34d3b0a
Travis Robson <travisjrobson@gmail.com>
2016-11-25 22:52:00 -0700

    done for the night

--- First_Assignment/studentt_sampler.c
+++ First_Assignment/studentt_sampler.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+
+int main(void) {
+	const gsl_rng_type * T;
+	gsl_rng * r;
+	
+	double nu = 3.0; /* The order for student-t dist */
+	int i, n = 10;
+	
+	r = gsl_rng_alloc(gsl_rng_mt19937);
+	gsl_rng_set(r,1); /* Sets the seed for generator r */
+	
+	for (i=0;i<n;i++) {
+		unsigned int k = gsl_ran_poisson(r,nu);
+		printf(" %u\n",k);
+	}
+	
+	printf("\n");
+	gsl_rng_free(r);
+	return 0;
+}
